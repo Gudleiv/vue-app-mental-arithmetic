4a50a42bd0804dc535ee27ec877fabcafa18800a
Gleb Gudleiv <glebgudleiv@gmail.com>
2021-02-25 19:45:09 +0300

    prepare for share repo

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Vue Mental Arithmetic
 
-## Project setup
+Example: [abacus-mental.web.app](https://abacus-mental.web.app/)
+
+### Install 
 ```
 npm install
 ```
+
+> Sounds for voicing numbers are not included in the repository
 
 ### Development setup
 ```
--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,7 @@ import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDPEwZfMhohlybJxMwf9vvsXOXH6eNPuBc',
-  authDomain: 'abacus-mental.firebaseapp.com',
-  databaseURL: 'https://abacus-mental-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'abacus-mental',
-  storageBucket: 'abacus-mental.appspot.com',
-  messagingSenderId: '490198139260',
-  appId: '1:490198139260:web:c8e338d89f46ad687d64f9',
-  measurementId: 'G-VPK1GQB87F',
-}
+const firebaseConfig = {} // Put here your firebase config
 
 firebase.initializeApp(firebaseConfig)
 
